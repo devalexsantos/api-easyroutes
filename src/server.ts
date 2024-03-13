99a6191447a980c1dcce143e2fb84dc01bb9ebfe
Alex Santos <devalexsantos@gmail.com>
2024-03-13 15:27:10 -0300

    change ports

--- src/server.ts
+++ src/server.ts
@@ -3,8 +3,8 @@ import { app } from './app'
 app
   .listen({
     host: '0.0.0.0',
-    port: 3000,
+    port: 3333,
   })
   .then(() => {
-    console.log('HTTP server is running on port 3000')
+    console.log('HTTP server is running on port 3333')
   })
