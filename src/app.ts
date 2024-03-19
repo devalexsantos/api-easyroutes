11b7592ea8bfa8ee76346d0d234074a32a92f8d7
Alex Santos <devalexsantos@gmail.com>
2024-03-19 14:51:08 -0300

    chore: add token secret api

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,22 @@
-import fastify from 'fastify'
+import fastify, { FastifyReply, FastifyRequest } from 'fastify'
 import { reposRoutes } from './http/controllers/repos/routes'
 import { usersRoutes } from './http/controllers/users/routes'
 import { categoriesRoutes } from './http/controllers/categories/routes'
 import { tagsRoutes } from './http/controllers/tags/routes'
 
 export const app = fastify()
+
+const TOKEN_SECRET = process.env.TOKEN_SECRET
+
+const validateToken = async (request: FastifyRequest, reply: FastifyReply) => {
+  const token = request.headers.authorization
+
+  if (token !== `Bearer ${TOKEN_SECRET}`) {
+    reply.code(401).send('Unauthorized')
+  }
+}
+
+app.addHook('preHandler', validateToken)
 
 app.register(reposRoutes)
 app.register(usersRoutes)
